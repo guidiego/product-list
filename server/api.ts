d5296f5b6da62640e0d47217977cf7d8e97fd34e
Guilherme Diego <guilherme.albino.francisco@gmail.com>
2020-07-14 16:32:10 -0300

    chore(project): add and fix lints

--- server/api.ts
+++ server/api.ts
@@ -1,12 +1,10 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { NextApiRequest, NextApiResponse } from 'next';
 import Router from '@koa/router';
 
 const router = new Router();
 
 const hello = (ctx) => {
   ctx.body = { name: 'John Doe' };
-}
+};
 
 router.get('/hello', hello);
 
--- server/index.ts
+++ server/index.ts
@@ -3,29 +3,29 @@ import Router from '@koa/router';
 import next from 'next';
 import api from './api';
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = new Koa()
-  const router = new Router()
+  const server = new Koa();
+  const router = new Router();
 
-  router.use('/api', api.routes())
+  router.use('/api', api.routes());
 
   router.all('(.*)', async (ctx) => {
-    await handle(ctx.req, ctx.res)
-    ctx.respond = false
-  })
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
 
   server.use(async (ctx, next) => {
-    ctx.res.statusCode = 200
-    await next()
-  })
+    ctx.res.statusCode = 200;
+    await next();
+  });
 
-  server.use(router.routes())
+  server.use(router.routes());
   server.listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`)
-  })
+    console.log(`> Ready on http://localhost:${port}`);
+  });
 });
