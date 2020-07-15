0a939d58bce03f5655f08d60ac24ef1da3617123
Guilherme Diego <guilherme.albino.francisco@gmail.com>
2020-07-15 17:20:41 -0300

    fix(typescript): check in svg inport

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,12 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 
+type SvgrComponent = React.StatelessComponent<React.SVGAttributes<SVGElement>>;
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare type AnyObject = Record<any, any>;
+
+declare module '*.svg' {
+  const value: SvgrComponent;
+  export default value;
+}
