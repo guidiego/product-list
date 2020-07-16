5bd508c6d77b5357be31018a5e50b44adb138790
Guilherme Diego <guilherme.albino.francisco@gmail.com>
2020-07-16 10:32:44 -0300

    enhance(home): add PageTitle component to Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from '~/components/Header';
+import PageTitle from '~/components/PageTitle';
 
 export const Home: React.FC = () => (
   <>
     <Header />
+    <PageTitle />
     <main style={{ height: '2000px' }} />
   </>
 );
